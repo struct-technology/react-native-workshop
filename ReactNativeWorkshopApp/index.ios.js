28777f246ee71fabed8da518b2f3d43678290b90
Matheus Santos <matheus.santos@struct.com.br>
2016-01-20 03:36:31 -0200

    index files update

--- ReactNativeWorkshopApp/index.ios.js
+++ ReactNativeWorkshopApp/index.ios.js
@@ -1,6 +1,6 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * React Native Workshop App
+ * https://github.com/struct-technology/react-native-workshop
  */
 'use strict';
 import React, {
