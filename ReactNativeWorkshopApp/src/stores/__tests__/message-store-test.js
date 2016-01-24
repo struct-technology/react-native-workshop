23e806bbecf10a5680cf4983d59b237e7b14a9a9
Sergio Oliveira <seocam@seocam.com>
2016-01-24 17:59:34 -0200

    Fixed tests Signed-off-by: Renan Valentin <renan.lifestyle@hotmail.com> Signed-off-by: Sergio Oliveira <seocam@seocam.com>

--- ReactNativeWorkshopApp/src/stores/__tests__/message-store-test.js
+++ ReactNativeWorkshopApp/src/stores/__tests__/message-store-test.js
@@ -1,17 +1,18 @@
+jest.autoMockOff();
 
-import MessageStore from '../message-store';
-import MessageActions from '../../actions/message-actions';
+var MessageStore = require('../message-store').default;
+var MessageActions = require('../../actions/message-actions').default;
 
 describe('Message Store', function() {
   it('has an intial state of empty messages', function() {
     expect(MessageStore.getState().messages.length).toBe(0);
   });
   it('can handle message updates', function(){
-  	MessageActions.updateMessages(['hello']);
-  	expect(MessageStore.getState().messages).toEqual(['hello']);
+    MessageActions.updateMessages(['hello']);
+    expect(MessageStore.getState().messages).toEqual(['hello']);
   });
   it('can handle message errors', function(){
-  	MessageActions.messagesFailed('error');
-  	expect(MessageStore.getState().errorMessage).toBe('error');
+    MessageActions.messagesFailed('error');
+    expect(MessageStore.getState().errorMessage).toBe('error');
   });
 });
